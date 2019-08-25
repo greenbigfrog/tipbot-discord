dad43241a7ce3a742c8cc8e6931ee12d10a7e20c
Jonathan B <greenbigfrog@gmail.com>
2019-08-25 15:20:03 +0200

    Add balance broadcast capability

--- src/discord/commands/balance.cr
+++ src/discord/commands/balance.cr
@@ -3,7 +3,9 @@ class Balance
   end
 
   def call(msg, ctx)
-    ctx[Discord::Client].create_message(msg.channel_id, "#{msg.author.username} has a confirmed balance of **#{TB::Data::Account.read(:discord, msg.author.id.to_u64.to_i64).balance(@coin)} #{@coin.name_short}**")
+    str = "#{msg.author.username} has a confirmed balance of **#{TB::Data::Account.read(:discord, msg.author.id.to_u64.to_i64).balance(@coin)} #{@coin.name_short}**"
+    str += "\n\nPlease pay attention to this important message:\n```tex\n$ #{@coin.balance_broadcast}```" if @coin.balance_broadcast
+    ctx[Discord::Client].create_message(msg.channel_id, str)
     yield
   end
 end
