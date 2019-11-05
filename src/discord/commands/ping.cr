d19b22c5ae0098ae50fe9b6bc81c29969fa48902
Jonathan B <greenbigfrog@gmail.com>
2019-11-05 14:05:37 +0100

    Make ping embed single line

--- src/discord/commands/ping.cr
+++ src/discord/commands/ping.cr
@@ -1,11 +1,11 @@
 class Ping
   def call(payload, ctx)
     client = ctx[Discord::Client]
-    latency = Discord::EmbedField.new("Latency", "#{(Time.utc_now - payload.timestamp).total_milliseconds} ms")
-    processing_time = Discord::EmbedField.new("Processed in", elapsed_text(Time.utc_now - ctx[Command].time))
+    latency = Discord::EmbedField.new("Latency", "#{(Time.utc_now - payload.timestamp).total_milliseconds} ms", true)
+    processing_time = Discord::EmbedField.new("Processed in", elapsed_text(Time.utc_now - ctx[Command].time), true)
     m = client.create_message(payload.channel_id, "Pong!", Discord::Embed.new(fields: [latency, processing_time]))
     return unless m.is_a?(Discord::Message)
-    roundtrip = Discord::EmbedField.new("Roundtrip", "#{(Time.utc_now - payload.timestamp).total_milliseconds} ms")
+    roundtrip = Discord::EmbedField.new("Roundtrip", "#{(Time.utc_now - payload.timestamp).total_milliseconds} ms", true)
     footer = Discord::EmbedFooter.new("Processing of the whole command took #{(Time.utc_now - ctx[Command].time).total_milliseconds} ms")
     client.edit_message(m.channel_id, m.id,
       "Pong!",
